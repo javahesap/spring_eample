96650df15b2f91ae6c3141720f1f25f62fc61221
Beşir AYDEMİR <besir.aydemir@hmb.gov.tr>
2025-01-13 14:40:58 +0300

    urunler ile ilgili sorgular eklendi

--- springorm/springorm/src/main/java/springorm/example/repostory/CategoryRepository.java
+++ springorm/springorm/src/main/java/springorm/example/repostory/CategoryRepository.java
@@ -1,9 +1,17 @@
 package springorm.example.repostory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import springorm.example.model.Category;
+import org.springframework.data.jpa.repository.Query;
 
+import springorm.example.model.Category;
+import springorm.example.model.Urun;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+	
+	@Query("SELECT c FROM Category c WHERE c.name = :name")
+    Optional<Category> findByName(String name);
 }
